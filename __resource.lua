372f69b4364eb7aaabd31ba5dccde4479e66df56
Fife3k <84279855+Fifek001@users.noreply.github.com>
2022-05-21 11:26:37 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'Automatyczna informacja o starcie serwera by kadeciok#1234'
+description 'Automatyczna informacja o starcie serwera by Fifeek#0002'
 
 client_script {
 }
